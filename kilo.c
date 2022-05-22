db536b831bd89000c1d616a5d9d6d48cb4cbd877
Wesley Barros <wesleybarros950@gmail.com>
2022-05-22 11:22:56 -0300

    Initial files
    Enabled raw mode

---
+++ kilo.c
@@ -0,0 +1,55 @@
+/* includer */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/* data */
+
+struct termios orig_termios;
+
+/* terminal */
+
+void die(const char* msg) {
+    perror(msg);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/* init */
+
+int main(int argc, char** argv) {
+    enableRawMode();
+
+    while (1) {
+        char c = 0;
+
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (c == 'q') break;
+    }
+
+    return 0;
+}
